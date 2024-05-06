9a0fffb53c37a0c241f38523b7ee1bce500d2923
Sidney Bowe <sidneyb1202@gmail.com>
2024-05-06 14:43:15 -0700

    backend setup

---
+++ backend/src/main/java/com/revature/SmileMart/SmileMartApplication.java
@@ -0,0 +1,13 @@
+package com.revature.SmileMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileMartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/revature/SmileMart/SmileMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.SmileMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
