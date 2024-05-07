ca64960e1ea4d6673c9785e61d1587c40d2b9825
danielri02 <danielistre02@gmail.com>
2024-05-07 16:01:36 -0500

    context fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Page } from './components/Pages/Page';
@@ -8,12 +7,15 @@ import { UserContext } from './contexts/UserContext';
 export const backend = (path?: string) => "http://localhost:8080" + path ? "/" + path : ""
 
 function App() {
+
+	const [user, setUser] = useState({}) // Currently logged in user
+
 	return (
 		<div className="App">
 			<header className="App-header"></header>
 
 			<BrowserRouter>
-				<UserContext.Provider value={{user: null, setUser: null}}>
+				<UserContext.Provider value={{ user: user, setUser: setUser }}>
 					<Routes>
 						<Route
 							path=""
--- frontend/src/components/Pages/Page.tsx
+++ frontend/src/components/Pages/Page.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react"
+import { ReactNode, useContext } from "react"
+import { UserContext } from "../../contexts/UserContext"
 
 
 interface Props {
@@ -6,6 +7,10 @@ interface Props {
 }
 
 export const Page: React.FC<Props> = ({ children }) => {
+
+	// Example usage. Should use directly in the component where needed
+	const {user, setUser} = useContext(UserContext)
+
 	return (
 		<div>
 			<main>{children}</main>
