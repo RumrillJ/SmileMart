87c783adc399274221650c603ebb9626daaf1799
danielri02 <danielistre02@gmail.com>
2024-05-08 15:19:37 -0500

    fixed order dao...

--- backend/src/main/java/com/revature/daos/OrderDAO.java
+++ backend/src/main/java/com/revature/daos/OrderDAO.java
@@ -1,8 +1,9 @@
 package com.revature.daos;
 
+import com.revature.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDAO extends JpaRepository<OrderDAO, Integer> {
+public interface OrderDAO extends JpaRepository<Order, Integer> {
 }
