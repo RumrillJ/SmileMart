9f9d585835962e28d826b4558920ab40afcc5ef8
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-09 15:40:50 -0500

    Get All Products, Add Products Backend User Story Completed

--- backend/src/main/java/com/revature/controllers/OrderController.java
+++ backend/src/main/java/com/revature/controllers/OrderController.java
@@ -9,13 +9,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping(value = "/orders")
 public class OrderController {
     private final OrderDAO orderDAO;
 
