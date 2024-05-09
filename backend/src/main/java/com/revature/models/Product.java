4db4abf3a27b9d2c03594ba3a592f84cd60339d1
wardalhadid <wf.hadid94@gmail.com>
2024-05-09 15:01:09 -0400

    completer user, category & product models

--- backend/src/main/java/com/revature/models/Product.java
+++ backend/src/main/java/com/revature/models/Product.java
@@ -20,4 +20,69 @@ public class Product {
     // clothing types: shoes, tops, headware, etc
     // demographic: men, women, kids
     // material: cotton, polyester, etc
+
+    private String name;
+
+    private double cost;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Category category;
+
+    // Constructors
+
+    public Product(int productId, String name, double cost, Category category) {
+        this.productId = productId;
+        this.name = name;
+        this.cost = cost;
+        this.category = category;
+    }
+
+    public Product() {
+    }
+
+    // Getters & Setters
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+
+    // To String
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", cost=" + cost +
+                ", category=" + category +
+                '}';
+    }
 }
