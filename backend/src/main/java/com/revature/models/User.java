02ec61c40e7728be02d81160d4d060dd8af5fac3
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-10 10:46:40 -0500

    first name, last name and address added

--- backend/src/main/java/com/revature/models/User.java
+++ backend/src/main/java/com/revature/models/User.java
@@ -18,7 +18,10 @@ public class User {
     private int userId;
 
     @Column(nullable = false)
-    private String name;
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
 
     @Column(nullable = false)
     private ROLE role;
@@ -29,16 +32,50 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    private String address;
+
+
+
+
+
     // generate getters and setters and constructor
-    public User(int userId, String name, ROLE role, String password, String email) {
+
+    public User(int userId, String firstName, String lastName, ROLE role, String password, String email, String address) {
         this.userId = userId;
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
         this.password = password;
         this.email = email;
+        this.address = address;
     }
 
     public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public int getUserId() {
@@ -49,13 +86,6 @@ public class User {
         this.userId = userId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public ROLE getRole() {
         return role;
@@ -85,10 +115,12 @@ public class User {
     public String toString() {
         return "User{" +
                 "userId=" + userId +
-                ", name='" + name + '\'' +
+                ", first name='" + firstName + '\'' +
+                ", last name='" + lastName+ '\'' +
                 ", role=" + role +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
                 '}';
     }
 }
