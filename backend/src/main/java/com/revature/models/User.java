ce137312515cfacb1a6921445ef0abd838460b9f
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-10 10:52:55 -0500

    city, state, zip and country fields added to user class

--- backend/src/main/java/com/revature/models/User.java
+++ backend/src/main/java/com/revature/models/User.java
@@ -35,13 +35,23 @@ public class User {
     @Column(nullable = false)
     private String address;
 
+    @Column(nullable = false)
+    private String city;
 
+    @Column(nullable = false)
+    private String state;
 
+    @Column(nullable = false)
+    private String zip;
+
+    @Column(nullable = false)
+    private String country;
 
 
     // generate getters and setters and constructor
 
-    public User(int userId, String firstName, String lastName, ROLE role, String password, String email, String address) {
+
+    public User(int userId, String firstName, String lastName, ROLE role, String password, String email, String address, String city, String state, String zip, String country) {
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -49,6 +59,10 @@ public class User {
         this.password = password;
         this.email = email;
         this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
     }
 
     public User() {
@@ -111,16 +125,52 @@ public class User {
         this.email = email;
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "userId=" + userId +
-                ", first name='" + firstName + '\'' +
-                ", last name='" + lastName+ '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 ", role=" + role +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
                 ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 }
