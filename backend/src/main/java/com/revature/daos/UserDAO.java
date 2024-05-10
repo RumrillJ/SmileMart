ea56bcd8a2d58090af7332a6e937a5ce3a13354c
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-10 12:33:30 -0500

    ProductServiceTest added

--- backend/src/main/java/com/revature/daos/UserDAO.java
+++ backend/src/main/java/com/revature/daos/UserDAO.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
-    public Optional<User> findByEmail(String email);
+     Optional<User> findByEmail(String email);
 }
