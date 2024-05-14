6dae927b308eb9d89f5d14c529164ec7a8f000df
wardalhadid <wf.hadid94@gmail.com>
2024-05-14 16:23:15 -0400

    enabled cors & changed setuser type to any

--- backend/src/main/java/com/revature/controllers/AuthenticationController.java
+++ backend/src/main/java/com/revature/controllers/AuthenticationController.java
@@ -5,15 +5,13 @@ import com.revature.models.dtos.UserRegistrationDTO;
 import com.revature.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthenticationController {
     private AuthenticationService authenticationService;
 
