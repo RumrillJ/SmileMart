b659eb87c53e9b8492c2b0dee5a8d226e826cbee
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-13 22:20:55 -0500

    CategoryController added

---
+++ backend/src/main/java/com/revature/controllers/CategoryController.java
@@ -0,0 +1,11 @@
+package com.revature.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    
+}
