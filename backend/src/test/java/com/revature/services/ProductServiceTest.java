b75b6ea9f3b9c34903e11d5a2f7588623da4caee
Chikaosolu Amaechi <amaechi.swe@gmail.com>
2024-05-13 21:52:27 -0500

    helping out

--- backend/src/test/java/com/revature/services/ProductServiceTest.java
+++ backend/src/test/java/com/revature/services/ProductServiceTest.java
@@ -6,13 +6,13 @@ import com.revature.daos.ProductDAO;
 import com.revature.models.Category;
 import com.revature.models.Product;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import org.mockito.MockitoAnnotations;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
