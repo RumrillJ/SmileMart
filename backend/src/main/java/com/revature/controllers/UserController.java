093a8e285c4779b5c96ec7b60f89dca981889a4a
yenj2 <yenj2015@gmail.com>
2024-05-14 14:33:18 -0600

    user story

--- backend/src/main/java/com/revature/controllers/UserController.java
+++ backend/src/main/java/com/revature/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.revature.controllers;
 
+import com.revature.models.Order;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,17 @@ public class UserController {
     public ResponseEntity<?> getAllUsers() {
         return ResponseEntity.ok().body("");
     }
-    
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getOrderByUser(@PathVariable int userId) { //return an order when user checkout
+        try {
+            Order order = userService.userCheckoutWithOrder(userId);
+            return ResponseEntity.ok(order);
+        }catch(RuntimeException e){
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+    }
+
+
+
 }
--- backend/src/main/java/com/revature/daos/UserDAO.java
+++ backend/src/main/java/com/revature/daos/UserDAO.java
@@ -1,10 +1,14 @@
 package com.revature.daos;
 
 
+import com.revature.models.Order;
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
+    List<Order> findOrdersByUserIdOrderByOrderIdDesc(int userId);
 }
