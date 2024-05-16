b76b888682a268c4cd84ee7cc5640628784db45e
JonCTack <jon.c.tackett@gmail.com>
2024-05-16 10:39:14 -0500

    updated default route, checkout button

--- frontend/src/components/Checkout/Checkout.tsx
+++ frontend/src/components/Checkout/Checkout.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export const Checkout: React.FC = () => {
 
     //TODO: have the order return on confirmation to database and set it in useState here or in useContext
-
+    const navigate = useNavigate();
     return(
         <div>
             <h2>Thank you for shopping with us!</h2>
             <p>Your order ID is: </p>
+            <button onClick={() => navigate("/")}>Back to Products</button>
         </div>
     )
 }
