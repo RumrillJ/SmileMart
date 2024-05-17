d052380dab10e45320fd5dbca94ec5d466fa7a02
Sidney Bowe <sidneyb1202@gmail.com>
2024-05-17 08:18:14 -0700

    logs in CategoryService

--- backend/src/main/java/com/revature/services/CategoryService.java
+++ backend/src/main/java/com/revature/services/CategoryService.java
@@ -2,12 +2,14 @@ package com.revature.services;
 
 import com.revature.daos.CategoryDAO;
 import com.revature.models.Category;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CategoryService {
 
     private final CategoryDAO categoryDAO;
@@ -19,7 +21,7 @@ public class CategoryService {
 
 
     public List<Category> getAllCategories() {
-
+        log.info("Retrieving all categories");
         return categoryDAO.findAll();
     }
 }
