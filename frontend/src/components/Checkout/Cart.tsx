f06daf566b9633e7115ab90afb0f7a5376ed80bb
JonCTack <jon.c.tackett@gmail.com>
2024-05-17 13:34:23 -0500

    some checkout logic

--- frontend/src/components/Checkout/Cart.tsx
+++ frontend/src/components/Checkout/Cart.tsx
@@ -1,16 +1,18 @@
+import { useNavigate } from "react-router-dom"
 import { useCart } from "../../contexts/CartContext"
 import { ProductItem } from "../Product/ProductItem"
 
+
 export const Cart: React.FC = () => {
 	const { cart } = useCart()
-
+	const navigate = useNavigate()
 	return (
 		<div>
 			<h2>Your Cart: </h2>
 			{Object.values(cart).map((product, index) => {
 				return <ProductItem product={product} key={"prd" + index}/>
 			})}
-			<button>Continue to checkout</button>
+			<button onClick={() => navigate("/checkout")}>Continue to checkout</button>
 		</div>
 	)
 }
